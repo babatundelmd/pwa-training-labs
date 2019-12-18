3aa39ddfa17f9e0f794f84ae6c8a5423230f69ee
Babatunde Lamidi <odunayohlamidi@gmail.com>
2019-12-18 13:00:30 +0100

    ✨chore: cache the application shell

---
+++ cache-api-lab/app/sw.js
@@ -0,0 +1,19 @@
+const filesToCache = [
+    '/',
+    'style/main.css',
+    'images/still_life_medium.jpg',
+    'index.html',
+    'pages/offline.html',
+    'pages/404.html'
+];
+
+const staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', event =>
+{
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => cache.addAll(filesToCache))
+    )
+})
