38f6b24a442ba900ac5bd87d8a4fe3239ceba3cb
Babatunde Lamidi <odunayohlamidi@gmail.com>
2019-12-18 10:32:05 +0100

    ⚡️chore: intercept network requests

--- service-worker-lab/app/service-worker.js
+++ service-worker-lab/app/service-worker.js
@@ -1,3 +1,6 @@
 //Listening for life cycle events
 self.addEventListener('install', event => console.log('Service worker installing...'));
 self.addEventListener('activate', event => console.log('Service worker activating...'));
+
+// Intercept network requests
+self.addEventListener('fetch', event => console.log('Fetching: ', event.request.url))
